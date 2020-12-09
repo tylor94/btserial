86b67a7fcf55af4be93d20418b6b1e13f550a634
Tylor <tnewman94@gmail.com>
2020-12-09 17:41:41 -0500

    Formatting and functionality updates

--- init_TNC2_2018.sh
+++ init_TNC2_2018.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Device variables
+# Bluetooth address
 address="20:19:04:03:20:18"
+# Service channel on bluetooth device
 channel="1"
+# Device pretty name
 name="TNC2 2018"
+# Device machine name
+# NO SPACES
 alias="tnc2_2018"
+# RFcomm port
 rfcomm="118"
 
 # Formatting variables
@@ -24,20 +30,23 @@ fi
 printf "$newline" ;\
 	printf "Starting watchdog... " ;\
 \
+# Create logfile if it doesn't exist already
+touch $alias.log ;\
+\
+# Connection Loop
 while true; do
 	# Remove old alias from /dev/
-	rm -f /dev/$alias >> watchdog.log 2>&1
+	rm -f /dev/$alias >> $alias.log 2>&1
 	# Create new alias in /dev/
-	ln -s rfcomm$rfcomm /dev/$alias >> watchdog.log 2>&1
+	ln -s rfcomm$rfcomm /dev/$alias >> $alias.log 2>&1
 	# Connect to device
-	rfcomm connect $rfcomm $address $channel >> watchdog.log 2>&1
+	rfcomm connect $rfcomm $address $channel >> $alias.log 2>&1
 	# Wait until rfcomm fails, then loop
 	wait
 # Break loop off into background process
 done &
 \
 # Print DONE message
-#printf "$newline" ;\
 	printf "DONE." ;\
 	printf "$newline" ;\
 \
--- kill_rfcomm.sh
+++ kill_rfcomm.sh
@@ -18,10 +18,29 @@ printf "$newline" ;\
 	printf "Killing RFCOMM... " ;\
 killall rfcomm &&\
 \
-# Restart bluetooth to dump connections
+# Turn bluetooth power off
+printf "$newline" ;\
+	printf "Turning bluetooth off... " ;\
+bluetoothctl power off &&\
+	sleep 1 ;\
+\
+# Stop bluetooth to dump connections
+printf "$newline" ;\
+	printf "Stopping bluetooth... " ;\
+systemctl stop bluetooth.service &&\
+\
+# Restart bluetooth
 printf "$newline" ;\
 	printf "Restarting bluetooth... " ;\
 systemctl restart bluetooth.service &&\
+\
+# Turn bluetooth power on
+printf "$newline" ;\
+	printf "Turning bluetooth on... " ;\
+# For some reason this command populates a bunch of uuids on screen each time it runs
+# Its output is sent to /dev/null to keep it quiet for now
+bluetoothctl power on >/dev/null 2>&1 &&\
+	sleep 1 ;\
 \
 # Print DONE message
 printf "$newline" ;\
