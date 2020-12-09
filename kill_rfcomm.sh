f2a3c830fca8d3f812cfd2c6b3f9c6ff447a46e3
Tylor <tnewman94@gmail.com>
2020-12-09 18:11:58 -0500

    Updated output handling on kill script

--- kill_rfcomm.sh
+++ kill_rfcomm.sh
@@ -20,23 +20,25 @@ killall rfcomm &&\
 \
 # Turn bluetooth power off
 printf "$newline" ;\
-	printf "Turning bluetooth off... " ;\
-bluetoothctl power off &&\
+	printf "Turning bluetooth power off... " ;\
+# This command will populate all the currently connected MAC addresses as they disconnect
+# I don't want to see that, so it goes to /dev/null for now
+bluetoothctl power off >/dev/null 2>&1 &&\
 	sleep 1 ;\
 \
-# Stop bluetooth to dump connections
+# Stop bluetooth service
 printf "$newline" ;\
-	printf "Stopping bluetooth... " ;\
+	printf "Stopping bluetooth service... " ;\
 systemctl stop bluetooth.service &&\
 \
-# Restart bluetooth
+# Restart bluetooth service
 printf "$newline" ;\
-	printf "Restarting bluetooth... " ;\
+	printf "Restarting bluetooth service... " ;\
 systemctl restart bluetooth.service &&\
 \
 # Turn bluetooth power on
 printf "$newline" ;\
-	printf "Turning bluetooth on... " ;\
+	printf "Turning bluetooth power on... " ;\
 # For some reason this command populates a bunch of uuids on screen each time it runs
 # Its output is sent to /dev/null to keep it quiet for now
 bluetoothctl power on >/dev/null 2>&1 &&\
