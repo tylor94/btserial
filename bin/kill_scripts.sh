fdf1d69eea0500a72576627d43fc44961195f130
Tylor <tnewman94@gmail.com>
2020-12-10 14:36:34 -0500

    Updating loops and connection handling. Ser2net still not working from command line.

---
+++ bin/kill_scripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Formatting variables
+padding="echo"
+newline="\n"
+
+# Check for root
+if [ "$EUID" -ne 0 ]
+	then
+		$padding
+		echo "This script requires root."
+		$padding
+	exit
+fi
+
+killall /bin/bash rfcomm socat
