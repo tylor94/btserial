9b68f8a610ee06801bf67eb3586209a4506687d6
Tylor <tnewman94@gmail.com>
2020-12-13 13:07:45 -0500

    Added sript to make symlinks

---
+++ bin/make_symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Formatting variables
+padding="echo"
+newline="\n"
+
+# Make symlinks from external confgs in folders
+ln -s /etc/aprx.conf ../conf/ ;\
+ln -s /etc/ser2net.conf ../conf/ ;\
+ln -s ../conf/* ./ ;\
