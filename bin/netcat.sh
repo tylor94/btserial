5367356f48be44ce7f06863624ed81688ed366ea
Tylor <tnewman94@gmail.com>
2020-12-14 19:24:15 -0500

    724

--- bin/netcat.sh
+++ bin/netcat.sh
@@ -18,7 +18,7 @@ fi
 
 # Variables are net address and net port and are fed in when executing script
 # Break off into own process
-cat /dev/rfcomm$1 | netcat -lp $2
+cat /dev/rfcomm$1 | netcat -lp $2 ;\
 # This will cat /def/rfcomm of choice to the network port of choice
 # Any resonses back in from the network currently go to stdout
 # This acts like a 1-way-read-serial adapter, in which nothing travels back to the tnc
