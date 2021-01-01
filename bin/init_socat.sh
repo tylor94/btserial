bb04b2bf776a16a8fa1e9a80fba89babba19e8c9
Tylor <tnewman94@gmail.com>
2021-01-01 18:40:57 -0500

    640

--- bin/init_socat.sh
+++ bin/init_socat.sh
@@ -23,7 +23,7 @@ touch ../var/socat_$2.pid ;\
 # Socat watchdog
 while true; do
 	# Kill xastir if socat fails. I don't know how else to restart xastir.
-	killall xastir
+	killall xastir >> ../log/socat_$2.log 2>&1
 	# Start socat
 	# Variables are net address and net port and are fed in when executing script
 	# Break off into own process
