e58e41a5fca392bba590b97e68cb8d25f3823a45
Tylor <tnewman94@gmail.com>
2021-01-01 18:22:26 -0500

    622

--- bin/init_socat.sh
+++ bin/init_socat.sh
@@ -13,6 +13,22 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
-# Variables are net address and net port and are fed in when executing script
-# Break off into own process
-socat pty,link=/dev/socat$2,raw tcp:$1:$2 &
+
+# Create logfile
+touch ../log/socat_$2.log ;\
+\
+# Create pidfile (currently unused)
+touch ../var/socat_$2.pid ;\
+\
+# Socat watchdog
+while true; do
+	# Start socat
+	# Variables are net address and net port and are fed in when executing script
+	# Break off into own process
+	socat pty,link=/dev/socat$2,raw tcp:$1:$2 >> ../log/socat_$2.log 2>&1
+	wait
+	# Wait before restarting loop
+	sleep 5
+	wait
+# Break loop into separate process
+done &\
