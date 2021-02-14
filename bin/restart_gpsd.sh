f99988fa287dbd0b46deec653e86a4b85356f30e
Tylor <tnewman94@gmail.com>
2021-02-14 00:01:52 -0500

    Restart GPSD script

---
+++ bin/restart_gpsd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Formatting variables
+padding="echo"
+newline="\n"
+
+# Check for root
+if [ "$EUID" -ne 0 ]
+	then
+		$padding
+		echo "This script requires root."
+		$padding
+	exit
+fi
+
+# Restart gpsd service
+systemctl restart gpsd.service ;
