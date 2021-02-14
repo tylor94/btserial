1caac8cc9ade385df4af5ab703656ddf8db1aec3
Tylor <tnewman94@gmail.com>
2021-02-14 01:21:39 -0500

    Added retstart_ser2net.sh script

---
+++ bin/restart_ser2net.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Formatting variables
+padding="echo"
+newline="\n"
+
+# Check for root
+if [ "$EUID" -ne 0 ]
+	then
+		$padding
+		echo "This script requires root."
+		$padding
+	exit
+fi
+
+# Restart ser2net service
+systemctl restart ser2net.service ;
